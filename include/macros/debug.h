7a0bf239cb5e0296a19951ca093fb1aff1a29f45
Adam <atuft89@gmail.com>
2021-04-16 13:31:45 +0100

    pull in macros-v0.2

--- include/macros/debug.h
+++ include/macros/debug.h
@@ -8,16 +8,16 @@
 #endif
 
 #define LOG_ERROR(fmt, ...)                      \
-    fprintf(stderr, "[E] (%s:%s:%d) " fmt "\n",  \
-        __FILE__, __func__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+    fprintf(stderr, "[E] " fmt " (%s:%d in %s)\n"  \
+        __VA_OPT__(,) __VA_ARGS__, __FILE__, __LINE__, __func__)
 
 #define LOG_ERROR_IF(pred, fmt, ...)             \
     do { if (pred) LOG_ERROR(fmt, __VA_ARGS__); } while(0)
 
 #if DEBUG_LEVEL > 0
 #define LOG_WARN(fmt, ...)                       \
-    fprintf(stderr, "[w] (%s:%s:%d) " fmt "\n",  \
-        __FILE__, __func__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+    fprintf(stderr, "[w] " fmt " (%s:%d in %s)\n"  \
+        __VA_OPT__(,) __VA_ARGS__, __FILE__, __LINE__, __func__)
 #define LOG_WARN_IF(pred, fmt, ...)              \
     do { if (pred) LOG_WARN(fmt, __VA_ARGS__); } while(0)
 #else
@@ -27,8 +27,8 @@
 
 #if DEBUG_LEVEL > 1
 #define LOG_INFO(fmt, ...)                       \
-    fprintf(stderr, "[i] (%s:%s:%d) " fmt "\n",  \
-        __FILE__, __func__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+    fprintf(stderr, "[i] " fmt " (%s:%d in %s)\n"  \
+        __VA_OPT__(,) __VA_ARGS__, __FILE__, __LINE__, __func__)
 #define LOG_INFO_IF(pred, fmt, ...)              \
     do { if (pred) LOG_INFO(fmt, __VA_ARGS__); } while(0)
 #else
@@ -38,8 +38,8 @@
 
 #if DEBUG_LEVEL > 2
 #define LOG_DEBUG(fmt, ...)                      \
-    fprintf(stderr, "[d] (%s:%s:%d) " fmt "\n",  \
-        __FILE__, __func__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+    fprintf(stderr, "[d] " fmt " (%s:%d in %s)\n"  \
+        __VA_OPT__(,) __VA_ARGS__, __FILE__, __LINE__, __func__)
 #define LOG_DEBUG_IF(pred, fmt, ...)             \
     do { if (pred) LOG_DEBUG(fmt, __VA_ARGS__); } while(0)
 #else

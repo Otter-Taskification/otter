d41a46c1db24ecfe12be42484e03a03b30031142
Adam <atuft89@gmail.com>
2021-04-16 17:08:16 +0100

    change to static mutex to reduce code, change debug messages, add clean-up goto in tt_new_node to ensure mutex released

--- src/modules/task-tree.c
+++ src/modules/task-tree.c
@@ -28,13 +28,14 @@ struct tt_node_t {
 /* Global task tree - singleton */
 static task_tree_t Tree = {
     .initialised = false,
-    .node_queue = NULL
+    .node_queue = NULL,
+    .lock = PTHREAD_MUTEX_INITIALIZER
 };
 
 void destroy_tree_node(void *ptr)
 {
     tt_node_t *node = (tt_node_t*) ptr;
-    LOG_DEBUG("callback: destroying tree node %p", node);
+    LOG_DEBUG("task tree callback: destroying tree node %p", node);
     da_destroy(node->children);
     free(node);
     return;
@@ -45,20 +46,14 @@ tt_init_tree(void)
 {
     if (Tree.initialised)
     {
-        LOG_WARN("tree already initialised");
+        LOG_WARN("task tree already initialised");
         return Tree.initialised;
     }
-    LOG_INFO("initialising task tree");
+    LOG_INFO("task tree initialising");
     if (NULL == (Tree.node_queue = qu_create(&destroy_tree_node)))
     {
-        LOG_ERROR("failed to initialise task tree, aborting");
+        LOG_ERROR("task tree failed to initialise, aborting");
         abort();
-    }
-    int mutex_init_err = 0;
-    if (0 != (mutex_init_err = pthread_mutex_init(&Tree.lock, NULL)))
-    {
-        LOG_ERROR("%s", strerror(mutex_init_err));
-        return Tree.initialised;
     }
     return Tree.initialised = true;
 }
@@ -75,7 +70,7 @@ tt_destroy_tree(void)
 {
     if (Tree.initialised == false)
     {
-        LOG_WARN("tree not initialised");
+        LOG_WARN("task tree not initialised");
         return;
     }
     LOG_INFO("destroying task tree");
@@ -89,30 +84,36 @@ tt_destroy_tree(void)
 tt_node_t *
 tt_new_node(tt_node_id_t parent_id, size_t n_children)
 {
-    tt_node_t *node;
+    pthread_mutex_lock(&Tree.lock);
+    tt_node_t *node = NULL;
+    if (false == Tree.initialised)
+    {
+        LOG_ERROR("task tree not initialised before adding node, aborting");
+        abort();
+    }
     if (NULL == (node = malloc(sizeof(*node))))
     {
         LOG_ERROR("failed to create node for parent %p", parent_id.ptr);
-        return NULL;
+        goto unlock_and_exit;
     }
     node->parent_id = parent_id;
     if (NULL == (node->children = da_create(n_children)))
     {
-        LOG_ERROR("failed to create dynamic array for parent %p",
+        LOG_ERROR("task tree failed to create dynamic array for parent %p",
             parent_id.ptr);
         free(node);
-        return NULL;
+        node = NULL;
+        goto unlock_and_exit;
     }
-
-    pthread_mutex_lock(&Tree.lock);
-    bool node_was_enqueued = qu_enqueue(
-            Tree.node_queue,(queue_data_t){.ptr = node});
-    LOG_ERROR_IF(false == node_was_enqueued,
-        "failed to add parent %p to task tree", parent_id.ptr);
-    pthread_mutex_unlock(&Tree.lock);
-
-    LOG_DEBUG("created node for parent %p with %lu elements",
+    LOG_DEBUG("task tree created node for parent %p with %lu elements",
         parent_id.ptr, n_children);
+    bool node_was_enqueued = qu_enqueue(
+            Tree.node_queue, (queue_data_t){.ptr = node});
+    LOG_ERROR_IF(false == node_was_enqueued,
+        "task tree failed to add parent %p", parent_id.ptr);
+
+unlock_and_exit:
+    pthread_mutex_unlock(&Tree.lock);
     return node;
 }
 
@@ -121,17 +122,17 @@ tt_add_child_to_node(tt_node_t *parent_node, tt_node_id_t child_id)
 {
     if (parent_node == NULL)
     {
-        LOG_ERROR("failed to add child %p: null node pointer", child_id.ptr);
+        LOG_ERROR("task tree failed to add child %p: null node pointer", child_id.ptr);
         return false;
     }
     if (false == da_push_back(parent_node->children,
         (array_element_t) {.ptr = child_id.ptr}))
     {
-        LOG_ERROR("failed to add child %p to parent node %p",
+        LOG_ERROR("task tree failed to add child %p to parent node %p",
             child_id.ptr, parent_node);
         return false;
     }
-    LOG_DEBUG("added child %p to node %p (len=%lu)",
+    LOG_DEBUG("task tree added child %p to node %p (len=%lu)",
         child_id.ptr, parent_node, da_get_length(parent_node->children));
     return true;
 }

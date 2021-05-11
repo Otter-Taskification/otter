9664d169381ad500656fab0520f7347e87ca9059
Adam <atuft89@gmail.com>
2021-05-11 21:59:37 +0100

    Update demo

--- src/otter-demo/omp-minimal-taskgroup.c
+++ src/otter-demo/omp-minimal-taskgroup.c
@@ -2,17 +2,18 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define THREADS 8
-#define LEN 2
+#define THREADS 2
+#define LEN 5
 
 int main(void)
 {
     int j=0;
     #pragma omp parallel num_threads(THREADS)
     #pragma omp single
-    #pragma omp taskgroup
+    #pragma omp taskloop nogroup
+    for (j=0; j<LEN; j++)
     {
-
+        usleep(10);
     }
 
     return 0;

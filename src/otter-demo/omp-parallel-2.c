6c40939406b652e1039a011d3d5944308d8000b2
Adam <atuft89@gmail.com>
2021-06-14 12:23:34 +0100

    Add new demo program for sequential parallel loops

---
+++ src/otter-demo/omp-parallel-2.c
@@ -0,0 +1,16 @@
+#include <omp.h>
+
+#define THREADS 2
+
+int main(void)
+{
+    #pragma omp parallel num_threads(THREADS)
+    {
+        // pass
+    }
+
+    #pragma omp parallel num_threads(THREADS*4)
+    {
+        // pass
+    }
+}

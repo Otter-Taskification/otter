f232e01802fc8713f51fac672b27a03ead7e7f4f
Adam <atuft89@gmail.com>
2021-07-15 09:08:40 +0100

    Add minimal setup.py for pip install -e; Update src/python/.gitignore

---
+++ src/python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(name="otter",
+      version="0.1",
+      description="Otter post-processing tool",
+      author="Adam Tuft",
+      author_email='adam.s.tuft@gmail.com',
+      platforms=["linux"],
+      license="https://github.com/adamtuft/otter/blob/main/LICENSE",
+      url="https://github.com/adamtuft/otter",
+      packages=find_packages(),
+      )

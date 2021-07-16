43c6f7048a23de07cba0c8175671ef359dafab56
Adam <atuft89@gmail.com>
2021-07-16 15:23:18 +0100

    Add parallel-for with task inside loop, update python .gitignore

---
+++ src/otter-demo/omp-parallel-for-task.c
@@ -0,0 +1,28 @@
+#include <omp.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define THREADS 2
+#define LEN 7
+
+int main(void)
+{
+    int num[LEN] = {0}, k=0;
+    omp_set_num_threads(THREADS);
+    #pragma omp parallel for
+    for (k=0; k<LEN; k++)
+    {
+        #pragma omp task
+        {
+            num[k] = omp_get_thread_num();
+            usleep(50);
+        }
+    }
+
+    for (k=0; k<LEN; k++)
+    {
+        printf("%d\n", num[k]);
+    } 
+
+    return 0;
+}

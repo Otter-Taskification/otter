02506cc2f14acd01509bac48c63069b375bfadad
Adam <atuft89@gmail.com>
2021-08-05 09:51:46 +0100

    Rearrange ompt-header to work better with Intel-Clang compiler

--- include/otter-ompt-header.h
+++ include/otter-ompt-header.h
@@ -1,10 +1,10 @@
 #if !defined(OTTER_OMPT_HEADER_H)
 #define OTTER_OMPT_HEADER_H
 
-#if defined(__INTEL_COMPILER)
-#include <omp-tools.h>
-#else
+#if defined(__clang__) && !(defined(__INTEL_COMPILER) || defined(__INTEL_CLANG_COMPILER))
 #include <ompt.h>
+#else
+#include <omp-tools.h>
 #endif
 
 #endif // OTTER_OMPT_HEADER_H

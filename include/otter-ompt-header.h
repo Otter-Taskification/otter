cf1df0ffac65eb0eb96fff2e70872d3b226f6727
Adam <atuft89@gmail.com>
2021-08-10 09:22:08 +0100

    Update ompt header to work with clang >= 12

--- include/otter-ompt-header.h
+++ include/otter-ompt-header.h
@@ -2,10 +2,13 @@
 #define OTTER_OMPT_HEADER_H
 
 #if defined(__clang__) && !(defined(__INTEL_COMPILER) || defined(__INTEL_CLANG_COMPILER))
-#include <ompt.h>
+    #include <ompt.h>
+    #if __clang_major__ >= 12
+    #define USE_OMPT_MASKED
+    #endif
 #else
-#define USE_OMPT_MASKED
-#include <omp-tools.h>
+    #define USE_OMPT_MASKED
+    #include <omp-tools.h>
 #endif
 
 #endif // OTTER_OMPT_HEADER_H

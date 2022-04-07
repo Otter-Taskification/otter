eab3f27a6c6e18efa63153c7ff92437719f71544
Adam <adam.s.tuft@gmail.com>
2022-04-07 20:28:35 +0100

    Add more taskgroup regions

--- src/otter/examples/omp-taskgroup-single.c
+++ src/otter/examples/omp-taskgroup-single.c
@@ -10,11 +10,27 @@ int main(void)
     int j=0;
     #pragma omp parallel num_threads(THREADS)
     #pragma omp single
-    #pragma omp taskgroup
-    for (j=0; j<LEN; j++)
     {
-        #pragma omp task
-        {usleep(10);}
+        #pragma omp taskgroup
+        for (j=0; j<LEN; j++)
+        {
+            #pragma omp task
+            {usleep(10);}
+        }
+
+        #pragma omp taskgroup
+        for (j=0; j<LEN; j++)
+        {
+            #pragma omp task
+            {usleep(10);}
+        }
+
+        #pragma omp taskgroup
+        for (j=0; j<LEN; j++)
+        {
+            #pragma omp task
+            {usleep(10);}
+        }
     }
 
     return 0;

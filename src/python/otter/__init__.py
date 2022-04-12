8ee02d67ade6aa840e99acad3a2c9f3d5bd6976b
Adam <73994535+adamtuft@users.noreply.github.com>
2022-04-12 12:55:15 +0100

    Remove src/python, replace with pyotter submodule at modules/pyotter

--- src/python/otter/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Key functionality:
-
-- load OTF2 trace and extract locations, events, regions, attributes so they can be easily looked up
-- parse loaded trace to determine what graph nodes to create and where edges are required
-- export execution graph to file
-"""

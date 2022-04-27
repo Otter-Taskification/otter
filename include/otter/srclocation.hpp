968d8249e2c6511737c9f3c9c7dc9d484d777562
adamtuft <adam.s.tuft@durham.ac.uk>
2022-04-27 14:34:04 +0100

    Initial prototype of source location map

--- include/otter/srclocation.hpp
+++ include/otter/srclocation.hpp
@@ -1,38 +1,75 @@
+#ifdef __cplusplus
 #include <ostream>
 #include <cstdint>
 #include <tuple>
 #include <map>
+#else
+#include <stdint.h>
+#endif
 
+#ifdef __cplusplus
 using std::ostream;
 
 namespace Source {
 
-using tag = std::tuple<const char *const, int, const char *const>;
+using source_tag = std::tuple<const char *const, int, const char *const>;
 
 // Opaque class represents the information needed for OTF2 to define a 
 // source location
-class obj;
+class object;
 
 // Opaque class which maps SrcLoc to SrcLocObj*
-class reg;
+class source_registry;
 
-reg *make_registry();
-void delete_registry(reg *);
+extern "C" {
+source_registry *source_registry_make();
+void source_registry_delete(source_registry *);
+}
 
 // Handle around opaque class SrcLocObj (PIMPL)
-class handle
+class source_handle
 {
 public:
-    handle(tag &src, reg &reg);
-    ~handle() = default;
+    source_handle(source_tag src, source_registry &reg);
+    source_handle(const char *file, int line, const char *func, source_registry &reg);
+    source_handle(const char *file, int line, const char *func, source_registry *reg);
+    ~source_handle() = default;
     uint64_t getID();
+    const char* get_file();
+    const char* get_func();
+    int get_line();
     ostream& stream(ostream&) const;
 private:
-    const obj *srcObj;
+    const object *srcObj;
 };
+
+extern "C" {
+source_handle *source_handle_make(const char*, int, const char*, source_registry*);
+void source_handle_delete(source_handle*);
+const char* source_handle_get_file(source_handle*);
+const char* source_handle_get_func(source_handle*);
+int source_handle_get_line(source_handle*);
+}
 
 };
 
 // Declare ostream operators
-ostream& operator<<(ostream& os, const Source::handle& h);
-ostream& operator<<(ostream&, Source::handle const&);
+ostream& operator<<(ostream& os, const Source::source_handle& h);
+ostream& operator<<(ostream&, Source::source_handle const&);
+
+#else
+
+typedef struct source_tag source_tag;
+typedef struct source_handle source_handle;
+typedef struct source_registry source_registry;
+
+source_registry *source_registry_make(void);
+void source_registry_delete(source_registry *);
+
+source_handle *source_handle_make(const char*, int, const char*, source_registry*);
+void source_handle_delete(source_handle*);
+const char* source_handle_get_file(source_handle*);
+const char* source_handle_get_func(source_handle*);
+int source_handle_get_line(source_handle*);
+
+#endif // #ifdef __cplusplus

21455fc09fa9dc599bf5ebb53051877ba6f84e13
Adam <73994535+adamtuft@users.noreply.github.com>
2022-05-20 23:36:49 +0100

    Add example using taskwait with otter_sync_descendants

---
+++ src/otter/examples/otter-serial-loop-taskwait.c
@@ -0,0 +1,49 @@
+#include "otter/otter-serial.h"
+
+static const int LEN=5;
+
+int main(void)
+{
+    otterTraceInitialise(OTTER_SRC_ARGS());
+    otterThreadsBegin(OTTER_SRC_ARGS());
+    {
+        otterLoopBegin(); for(int i=0; i<LEN; i++)
+        {
+            otterTaskBegin(OTTER_SRC_ARGS());
+            {
+                otterTaskBegin(OTTER_SRC_ARGS());
+                otterTaskEnd();
+
+                otterTaskBegin(OTTER_SRC_ARGS());
+                otterTaskEnd();
+                
+                otterTaskBegin(OTTER_SRC_ARGS());
+                otterTaskEnd();
+            }
+            otterTaskEnd();
+        }
+        otterLoopEnd();
+        
+        otterLoopBegin(); for(int i=0; i<LEN; i++)
+        {
+            otterTaskBegin(OTTER_SRC_ARGS());
+            otterTaskEnd();
+        }
+        otterLoopEnd();
+
+        otterSynchroniseTasks(otter_sync_descendants);
+        
+        otterLoopBegin(); for(int i=0; i<LEN; i++)
+        {
+            otterTaskBegin(OTTER_SRC_ARGS());
+            otterTaskEnd();
+        }
+        otterLoopEnd();
+
+        otterSynchroniseTasks(otter_sync_children);
+    }
+    otterThreadsEnd();
+    otterTraceFinalise();
+
+    return 0;
+}

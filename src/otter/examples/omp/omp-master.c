fb78a905d479281d82b194bfbcdc8e53f2381281
Adam <73994535+adamtuft@users.noreply.github.com>
2022-06-30 14:29:42 +0100

    Update validation example

--- src/otter/examples/omp/omp-master.c
+++ src/otter/examples/omp/omp-master.c
@@ -1,22 +1,41 @@
 #include <omp.h>
 #include <stdio.h>
 
-#define THREADS     4
-#define LOOPS       6
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s num_threads num_tasks\n", argv[0]);
+        return 1;
+    }
 
-int main(void) {
+    int threads = atoi(argv[1]);
+    int tasks = atoi(argv[2]);
 
-    #pragma omp parallel num_threads(4)
+    #pragma omp parallel num_threads(threads)
     {
         #pragma omp master
         {
             int j=0;
             printf("[%d] I am the master thread!\n", omp_get_thread_num());
             #pragma omp taskloop nogroup
-            for (j=0; j<LOOPS; j++)
+            for (j=0; j<tasks; j++)
             {
                 printf("[%d] loop %d\n", omp_get_thread_num(), j);
             }
         }
+
+        #pragma omp barrier
+
+        #pragma omp master
+        {
+            int j=0;
+            printf("[%d] I am the master thread!\n", omp_get_thread_num());
+            #pragma omp taskloop nogroup
+            for (j=0; j<tasks; j++)
+            {
+                printf("[%d] loop %d\n", omp_get_thread_num(), j);
+            }
+        }
+
     }
+
 }

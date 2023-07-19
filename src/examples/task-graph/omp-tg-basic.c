06037560a45fe712ee8a864646a556d916a9466a
Adam <73994535+adamtuft@users.noreply.github.com>
2023-07-19 13:04:54 +0100

    Add examples

---
+++ src/examples/task-graph/omp-tg-basic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <omp.h>
+#define OTTER_TASK_GRAPH_ENABLE_USER
+#include "api/otter-task-graph/otter-task-graph-user.h"
+
+int main(void) {
+
+    OTTER_INITIALISE();
+
+    OTTER_DECLARE_HANDLE(task);
+
+    #pragma omp parallel
+    {
+        OTTER_INIT_TASK(task, OTTER_NULL_TASK, 42, otter_no_add_to_pool, "task");
+        fprintf(stderr, "%d\n", omp_get_thread_num());
+        OTTER_TASK_START(task);
+        OTTER_TASK_END(task);
+    }
+
+    OTTER_FINALISE();
+
+    return 0;
+}

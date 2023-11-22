dbeb6d8ee7d0d58141ce1bd03b53947099f63188
Adam <73994535+adamtuft@users.noreply.github.com>
2023-11-22 09:09:11 +0000

    Add debug statements to help diagnose use-after-free errors

--- src/otter-trace/trace-task-context.c
+++ src/otter-trace/trace-task-context.c
@@ -33,8 +33,9 @@ struct otter_task_context {
 };
 
 otter_task_context *otterTaskContext_alloc(void) {
-  LOG_DEBUG("allocate task context");
-  return malloc(sizeof(otter_task_context));
+  otter_task_context *task = malloc(sizeof(otter_task_context));
+  LOG_DEBUG("allocate task context %p", task);
+  return task;
 }
 
 static unique_id_t unique_id = 0;
@@ -51,11 +52,11 @@ void otterTaskContext_init(otter_task_context *task, otter_task_context *parent,
   } else {
     task->parent_task_context_id = parent->task_context_id;
   }
-  LOG_DEBUG("initialised task context: %lu", task->task_context_id);
+  LOG_DEBUG("initialised task context %p: %lu", task, task->task_context_id);
 }
 
 void otterTaskContext_delete(otter_task_context *const task) {
-  LOG_DEBUG("delete task context: %lu", task->task_context_id);
+  LOG_DEBUG("delete task context %p: %lu", task, task->task_context_id);
   free(task);
 }
 
@@ -63,6 +64,7 @@ void otterTaskContext_delete(otter_task_context *const task) {
 
 unique_id_t
 otterTaskContext_get_task_context_id(const otter_task_context *task) {
+  LOG_DEBUG("otterTaskContext_get_task_context_id %p", task);
   // ! HACK ! only here to temporarily work around ExaHyPE not managing task
   // pointers assert(task != NULL);
   return task == NULL ? 0 : task->task_context_id;
@@ -70,26 +72,31 @@ otterTaskContext_get_task_context_id(const otter_task_context *task) {
 
 unique_id_t
 otterTaskContext_get_parent_task_context_id(const otter_task_context *task) {
+  LOG_DEBUG("otterTaskContext_get_parent_task_context_id %p", task);
   // assert(task != NULL);
   return task == NULL ? 0 : task->parent_task_context_id;
 }
 
 int otterTaskContext_get_task_flavour(const otter_task_context *task) {
+  LOG_DEBUG("otterTaskContext_get_task_flavour %p", task);
   return task == NULL ? INT_MAX : task->flavour;
 }
 
 otter_src_ref_t
 otterTaskContext_get_init_location_ref(const otter_task_context *task) {
+  LOG_DEBUG("otterTaskContext_get_init_location_ref %p", task);
   return task == NULL ? (otter_src_ref_t){0, 0, 0} : task->init_location;
 }
 
 otter_string_ref_t
 otterTaskContext_get_task_label_ref(const otter_task_context *task) {
+  LOG_DEBUG("otterTaskContext_get_task_label_ref %p", task);
   return task == NULL ? OTTER_STRING_UNDEFINED : task->label;
 }
 
 void otterTaskContext_set_task_label_ref(otter_task_context *task,
                                          otter_string_ref_t label) {
+  LOG_DEBUG("otterTaskContext_set_task_label_ref %p", task);
   if (task != NULL)
     task->label = label;
 }

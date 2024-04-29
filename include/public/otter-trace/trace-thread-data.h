6fc056606b094bfd27411ef7fb9687edca3cd752
Adam <73994535+adamtuft@users.noreply.github.com>
2024-04-29 17:02:00 +0100

    Add field for a thread to store its active task

--- include/public/otter-trace/trace-thread-data.h
+++ include/public/otter-trace/trace-thread-data.h
@@ -1,6 +1,7 @@
 #ifndef OTTER_TRACE_THREAD_DATA_H
 #define OTTER_TRACE_THREAD_DATA_H
 
+#include "api/otter-task-graph/otter-task-graph.h" // for otter_task_context typedef
 #include "public/otter-common.h"
 #include "public/otter-trace/trace-location.h"
 #include "public/otter-trace/trace-state.h"
@@ -8,10 +9,26 @@
 
 // TODO: can this struct be made opaque?
 typedef struct thread_data_t {
-  unique_id_t id;
-  trace_location_def_t *location;
-  otter_thread_t type;
-  bool is_master_thread; // of parallel region
+    unique_id_t id;
+    trace_location_def_t *location;
+    otter_thread_t type;
+    bool is_master_thread; // of parallel region
+
+    /**
+     * @brief Records the handle corresponding to the task being executed by the thread. NULL means
+     * the task is not currently executing an annotated task i.e. it is in the implicit task.
+     *
+     * Invariants:
+     *  - at any point, this thread-local variable represents the handle of the task presently being
+     *      executed.
+     *  - if NULL, no task is being executed.
+     *  - when the task being executed is changed, the thread must be notified and the active task
+     *      updated.
+     *  - the thread does *not* remember any other task, such as a task switched away from, or a parent
+     *      task.
+     *
+     */
+    otter_task_context *active_task;
 } thread_data_t;
 
 thread_data_t *new_thread_data(otter_thread_t type);
--- src/otter-trace/trace-thread-data.c
+++ src/otter-trace/trace-thread-data.c
@@ -1,24 +1,31 @@
+#include <inttypes.h>
+#include <stdlib.h>
+
+#define OTTER_USE_PRIVATE_HEADER
 #include "public/otter-trace/trace-thread-data.h"
 #include "trace-get-unique-id.h"
 #include "trace-static-constants.h"
-#include <stdlib.h>
 
 thread_data_t *new_thread_data(otter_thread_t type) {
-  thread_data_t *thread_data = malloc(sizeof(*thread_data));
-  *thread_data = (thread_data_t){.id = get_unique_id(),
-                                 .location = NULL,
-                                 .type = type,
-                                 .is_master_thread = false};
+    thread_data_t *thread_data = malloc(sizeof(*thread_data));
+    *thread_data = (thread_data_t){
+        .id = get_unique_id(),
+        .location = NULL,
+        .type = type,
+        .is_master_thread = false,
+        .active_task = NULL, // threads start out without holding any specific task
+    };
 
-  /* Create a location definition for this thread */
-  thread_data->location = trace_new_location_definition(
-      thread_data->id, type, OTF2_LOCATION_TYPE_CPU_THREAD,
-      DEFAULT_LOCATION_GRP);
+    /* Create a location definition for this thread */
+    thread_data->location =
+        trace_new_location_definition(thread_data->id, type, OTF2_LOCATION_TYPE_CPU_THREAD, DEFAULT_LOCATION_GRP);
 
-  return thread_data;
+    LOG_INFO("create thread %" PRIu64, thread_data->id);
+
+    return thread_data;
 }
 
 void thread_destroy(thread_data_t *thread_data) {
-  trace_destroy_location(thread_data->location);
-  free(thread_data);
+    trace_destroy_location(thread_data->location);
+    free(thread_data);
 }
